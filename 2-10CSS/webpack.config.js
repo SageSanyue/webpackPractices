0bc9179480e073b6b2736ce4d455b10e9c63fcba
SageSanyue <562441461@qq.com>
2020-03-21 00:26:40 +0800

    配置MiniCssExtractPlugin

--- 2-10CSS/webpack.config.js
+++ 2-10CSS/webpack.config.js
@@ -1,18 +1,33 @@
 const path = require('path')
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
     output: {
-        filename: 'main.js',
+        // 添加hash可以防止文件缓存，每次都会生成4位的hash串
+        filename: 'bundle.[hash:4].js',   
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 用哪个html作为模板
+            // 在src目录下创建一个index.html页面当做模板来用
+            template: 'index.html',
+            hash: true, // 会在打包好的bundle.js后面加上hash串
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
 }
