ccbd0459b9787475a5cd6b9032ac42d6a7a54453
SageSanyue <562441461@qq.com>
2020-03-26 00:12:39 +0800

    引入模块懒加载

--- immoc-frame-project-webpack/src/index.js
+++ immoc-frame-project-webpack/src/index.js
@@ -7,6 +7,10 @@ import { sum } from './math'
 const sumRes = sum(10, 20)
 console.log('sumRes', sumRes)
 
+// 引入第三方模块
+import _ from 'lodash'
+console.log(_.each)
+
 // // 增加，开启热更新之后的代码逻辑
 // if (module.hot) {
 //     module.hot.accept(['./math'], () => {
@@ -14,3 +18,10 @@ console.log('sumRes', sumRes)
 //         console.log('sumRes in hot', sumRes)
 //     })
 // }
+
+// 引入动态数据-懒加载
+setTimeout(() => {
+    import('./dynamic-data.js').then(res => {
+        console.log(res.default.message)
+    })
+}, 1500)
